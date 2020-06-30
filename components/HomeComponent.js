c07414ef4206548cc11a2d1a648369e919f3ee01
Pentti <poinonen0@gmail.com>
2020-06-30 10:47:44 +0300

    react navigation part3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,15 +1,47 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+const RenderItem = ({item}) => {
+  return (
+    item
+    ? 
+      <Card
+        featuredTitle={item.name}
+        featuredSubtitle={item.designation}
+        image={require('./images/uthappizza.png')}
+      >
+        <Text style={{margin: 10}}>
+          {item.description}
+        </Text>
+      </Card>
+    : ""
+  );
+};
 
 const Home = () => {
+
+  const [data] = useState({
+    dishes: DISHES,
+    promotions: PROMOTIONS,
+    leaders: LEADERS
+  });
+
   return (
-
-    <View>
-      <Text>
-        Home Component
-      </Text>
-    </View>
-
+    <ScrollView>
+      <RenderItem
+        item={data.dishes.filter(dish => dish.featured)[0]}
+      />
+      <RenderItem
+        item={data.leaders.filter(leader => leader.featured)[0]}
+      />
+      <RenderItem
+        item={data.promotions.filter(promotion => promotion.featured)[0]}
+      />
+    </ScrollView>
   );
 };
 
